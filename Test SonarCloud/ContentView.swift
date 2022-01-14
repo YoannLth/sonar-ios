ff1b98b5cf500784eb4af109597d81af256238ab
yoann <ylathuiliere@sqli.com>
2022-01-14 15:40:00 +0100

    First commit

---
+++ Test SonarCloud/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test SonarCloud
+//
+//  Created by Yoann Lathuliere on 14/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test SonarCloud/Test_SonarCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SonarCloudApp.swift
+//  Test SonarCloud
+//
+//  Created by Yoann Lathuliere on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_SonarCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
